2fb9bc08e6799392b3415f11b7351d3fb643b02f
epainter <gavgavov@gmail.com>
2022-08-02 23:29:54 +0300

    guard spawn

--- Assets/Scripts/PlantAttack.cs
+++ Assets/Scripts/PlantAttack.cs
@@ -14,13 +14,20 @@ public class PlantAttack : MonoBehaviour
     public float attackCooldown;
     public Transform spawnPoint;
     private float lastAttackTime;
+    private Transform nextTarget;
+    private float nextAngle;
+    private float currentAngle;
     private void Awake()
     {
         CalcAnims();
+        currentAngle = transform.rotation.eulerAngles.y;
+        nextTarget = attackTargets[0];
     }
 
     void Update()
     {
+        var y = Mathf.Lerp(currentAngle, nextAngle, (Time.time - lastAttackTime) / attackCooldown);
+        transform.rotation = Quaternion.Euler(new Vector3(0, y, 0));
         if (lastAttackTime < Time.time - attackCooldown)
         {
             Attack();
@@ -30,11 +37,15 @@ public class PlantAttack : MonoBehaviour
     private void Attack()
     {
         lastAttackTime = Mathf.Floor(Time.time / attackCooldown) * attackCooldown + attackCooldown * animationNormalizedOffset;
-        var target = attackTargets[Random.Range(0, attackTargets.Count)];
+        var target = nextTarget;
+        nextTarget = attackTargets[Random.Range(0, attackTargets.Count)];
+        transform.LookAt(nextTarget);
+        nextAngle = transform.rotation.eulerAngles.y;
         transform.LookAt(target);
         Vector3 eulerAngles = transform.rotation.eulerAngles;
         eulerAngles.x = 0;
         eulerAngles.z = 0;
+        currentAngle = eulerAngles.y;
         transform.rotation = Quaternion.Euler(eulerAngles);
         var bullet = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody>().velocity =
--- Assets/Scripts/UnitBuy.cs
+++ Assets/Scripts/UnitBuy.cs
@@ -1,14 +1,38 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitBuy : MonoBehaviour
 {
+    [Serializable]
+    public struct SpawnInfo
+    {
+        public KeyCode code;
+        public Spawner spawner;
+        public int moneyForSpawn;
+    }
     public LayerMask raycastTarget;
-    public Spawner spawner;
-    public int moneyForSpawn;
+    public List<SpawnInfo> spawners;
+    private Spawner spawner;
+    private int moneyForSpawn;
     public Camera cameraMain;
+
+    private void Awake()
+    {
+        spawner = spawners[0].spawner;
+        moneyForSpawn = spawners[0].moneyForSpawn;
+    }
+
     private void Update()
     {
+        foreach (var spawnInfo in spawners)
+        {
+            if (Input.GetKeyDown(spawnInfo.code))
+            {
+                spawner = spawnInfo.spawner;
+                moneyForSpawn = spawnInfo.moneyForSpawn;
+            }
+        }
         if (Input.GetMouseButtonUp(1))
         {
             if (MoneyManager.Instance.TrySpend(moneyForSpawn))
